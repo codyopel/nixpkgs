2bcc4ffbb275f121ee1b6791accd6d9ea8bfeeb6
codyopel <codyopel@gmail.com>
2015-02-09 19:22:29 -0500

    xfe: refactor

--- pkgs/applications/misc/xfe/default.nix
+++ pkgs/applications/misc/xfe/default.nix
@@ -1,31 +1,50 @@
-{ stdenv, fetchurl, fox, pkgconfig, gettext, x11, gcc, intltool, file, libpng }:
+{ stdenv, fetchurl, fox, freetype, gcc, gettext, pkgconfig, intltool, file, libpng, x11
+, xrandrSupport ? false, xrandr ? null # XRandR Support
+, snSupport ? true # Starup notification support
+}:
 
+# Segmentation fault error
+
+let
+  mkFlag = optSet: flag: if optSet then "--enable-${flag}" else "--disable-${flag}";
+in
+
+with stdenv.lib;
 stdenv.mkDerivation rec {
-  name = "xfe-1.37";
+  name = "xfe-${version}";
+  version = "1.40";
 
   src = fetchurl {
     url = "mirror://sourceforge/xfe/${name}.tar.gz";
-    sha256 = "1g9a0bpny2m7ixgxpqjh0wvh2x6d0lpj6682zn5dfqwan4j2xfsd";
+    sha256 = "1a32d2in4s25hr4znqvchs5sgv3wpdcnbknlbpl1q6pzpqanyirb";
   };
 
-  buildInputs = [ fox pkgconfig gettext x11 gcc intltool file libpng ];
-
-  preConfigure = ''
-    sed -i s,/usr/share/xfe,$out/share/xfe, src/xfedefs.h
+  # Fix hardcoded icon path
+  patchPhase = ''
+    sed -e 's,/usr/share/xfe,$out/share/xfe,' -i src/xfedefs.h
   '';
+
+  configureFlags = [
+    (mkFlag snSupport "sn")
+  ] ++ optional xrandrSupport "--with-xrandr";
+
+  nativeBuildInputs = [ pkgconfig ];
+
+  buildInputs = [ fox freetype gettext gcc intltool file libpng x11 ]
+    ++ optional xrandrSupport xrandr;
 
   enableParallelBuilding = true;
 
   meta = {
     description = "MS-Explorer like file manager for X";
+    homepage    = http://roland65.free.fr/xfe/;
+    license     = licenses.gpl2;
+    maintainers = with maintainers; [ bbenoist codyopel ];
+    platforms   = platforms.linux;
     longDescription = ''
       X File Explorer (Xfe) is an MS-Explorer like file manager for X.
       It is based on the popular, but discontinued, X Win Commander, which was developed by Maxim Baranov.
       Xfe aims to be the filemanager of choice for all the Unix addicts!
     '';
-    homepage = "http://sourceforge.net/projects/xfe/";
-    license = stdenv.lib.licenses.gpl2;
-    maintainers = [ stdenv.lib.maintainers.bbenoist ];
-    platforms = stdenv.lib.platforms.linux;
   };
 }
