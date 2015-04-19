e825ca2175b27c965003f6e2c2403e2b604e62b2
codyopel <codyopel@gmail.com>
2015-04-19 03:12:21 -0400

    stash changes

--- pkgs/applications/video/mpv/default.nix
+++ pkgs/applications/video/mpv/default.nix
@@ -1,62 +1,58 @@
-{ stdenv, fetchurl, fetchgit, freetype, pkgconfig, freefont_ttf, ffmpeg, libass
-, lua, perl, libpthreadstubs
-, lua5_sockets
-, python3, docutils, which, lib
+{ stdenv, fetchurl, perl, pkgconfig, python3
+, docutils, ffmpeg, freefont_ttf, freetype, libass, libpthreadstubs, lua, lua5_sockets, zlib
 , x11Support ? true, libX11 ? null, libXext ? null, mesa ? null, libXxf86vm ? null
 , xineramaSupport ? true, libXinerama ? null
-, xvSupport ? true, libXv ? null
-, sdl2Support? true, SDL2 ? null
-, alsaSupport ? true, alsaLib ? null
-, screenSaverSupport ? true, libXScrnSaver ? null
-, vdpauSupport ? true, libvdpau ? null
-, dvdreadSupport? true, libdvdread ? null
-, dvdnavSupport ? true, libdvdnav ? null
-, bluraySupport ? true, libbluray ? null
-, speexSupport ? true, speex ? null
-, theoraSupport ? true, libtheora ? null
-, jackaudioSupport ? true, jack2 ? null
-, pulseSupport ? true, pulseaudio ? null
-, bs2bSupport ? false, libbs2b ? null
 # For screenshots
-, libpngSupport ? true, libpng ? null
 # for Youtube support
-, youtubeSupport ? false, youtubeDL ? null
+, alsaLib ? null
+, bs2bSupport ? false, libbs2b ? null
 , cacaSupport ? false, libcaca ? null
+, jack2 ? null
+, libbluray ? null
+, libdvdnav ? null
+, libdvdread ? null
+, libpng ? null
+, libtheora ? null
+, libvdpau ? null
+, libXScrnSaver ? null
+, libXv ? null
+, pulseaudio ? null
+, SDL2 ? null
+, speex ? null
 , vaapiSupport ? false, libva ? null
+, youtubeSupport ? false, youtubeDL ? null
 }:
 
 assert x11Support -> (libX11 != null && libXext != null && mesa != null && libXxf86vm != null);
 assert xineramaSupport -> (libXinerama != null && x11Support);
-assert xvSupport -> (libXv != null && x11Support);
-assert sdl2Support -> SDL2 != null;
-assert alsaSupport -> alsaLib != null;
-assert screenSaverSupport -> libXScrnSaver != null;
-assert vdpauSupport -> libvdpau != null;
-assert dvdreadSupport -> libdvdread != null;
-assert dvdnavSupport -> libdvdnav != null;
-assert bluraySupport -> libbluray != null;
-assert speexSupport -> speex != null;
-assert theoraSupport -> libtheora != null;
-assert jackaudioSupport -> jack2 != null;
-assert pulseSupport -> pulseaudio != null;
 assert bs2bSupport -> libbs2b != null;
-assert libpngSupport -> libpng != null;
 assert youtubeSupport -> youtubeDL != null;
 assert cacaSupport -> libcaca != null;
 
-# Purity problem: Waf needed to be is downloaded by bootstrap.py
-# but by purity reasons it should be avoided; thanks the-kenny to point it out!
-# Now, it will just download and package Waf, mimetizing bootstrap.py behaviour
-
 let
+  /* Waf needs to be downloaded by bootstrap.py but for purity reasons
+     it should be avoided.  This will download and package Waf, mimetizing
+     bootstrap.py's behaviour. */
+
+     /*Since b23dbb1, if buildInputs contains
+a plain file it is used as a setup hook. The waf script which is used
+here in mpv however isn't a setup hook and also shouldn't be included in
+buildInputs as it was kind of a no-op before already.
+
+Failed build log:
+
+https://headcounter.org/hydra/build/582548/nixlog/1/raw
+
+Signed-off-by: aszlig <aszlig@redmoonstudios.org>*/
   waf = fetchurl {
-    url = http://ftp.waf.io/pub/release/waf-1.8.5;
+    url = "http://ftp.waf.io/pub/release/waf-1.8.5";
     sha256 = "0gh266076pd9fzwkycskyd3kkv2kds9613blpxmn9w4glkiwmmh5";
   };
 
+  inherit (stdenv) isCygwin isDarwin isLinux;
+  inherit (stdenv.lib) enableFeature optional optionals optionalString;
 in
 
-with stdenv.lib;
 stdenv.mkDerivation rec {
   name = "mpv-${version}";
   version = "0.8.3";
@@ -66,53 +62,64 @@ stdenv.mkDerivation rec {
     sha256 = "1kw9hr957cxqgm2i94bgqc6sskm6bwhm0akzckilhs460b43h409";
   };
 
-  buildInputs = 
-    [ python3 lua perl freetype pkgconfig ffmpeg libass docutils which libpthreadstubs lua5_sockets ]
-    ++ optionals x11Support [ libX11 libXext mesa libXxf86vm ]
-    ++ optional alsaSupport alsaLib
-    ++ optional xvSupport libXv
-    ++ optional theoraSupport libtheora
+  patchPhase = ''
+    patchShebangs ./TOOLS/
+  '';
+
+  configureFlags = [
+    "--enable-manpage-build"
+    "--enable-libmpv-shared"
+  ] ++ optional vaapiSupport "--enable-vaapi";
+
+  configurePhase = "python3 ${waf} configure --prefix=$out $configureFlags";
+
+  nativeBuildInputs = [ perl pkgconfig python3 ];
+
+  buildInputs = [
+    alsaLib
+    docutils
+    ffmpeg
+    freetype
+    jack2
+    libass
+    libbluray
+    libbs2b
+    libcaca
+    libdvdread
+    libdvdnav
+    libpng
+    libpthreadstubs
+    libva
+    libvdpau
+    libtheora
+    libXScrnSaver
+    libXv
+    lua
+    lua5_sockets
+    pulseaudio
+    SDL2
+    speex
+    youtubeDL
+    zlib
+  ] ++ optionals x11Support [ libX11 libXext mesa libXxf86vm ]
     ++ optional xineramaSupport libXinerama
-    ++ optional dvdreadSupport libdvdread
-    ++ optionals dvdnavSupport [ libdvdnav libdvdnav.libdvdread ]
-    ++ optional bluraySupport libbluray
-    ++ optional jackaudioSupport jack2
-    ++ optional pulseSupport pulseaudio
-    ++ optional screenSaverSupport libXScrnSaver
-    ++ optional vdpauSupport libvdpau
-    ++ optional speexSupport speex
-    ++ optional bs2bSupport libbs2b
-    ++ optional libpngSupport libpng
-    ++ optional youtubeSupport youtubeDL
-    ++ optional sdl2Support SDL2
-    ++ optional cacaSupport libcaca
-    ++ optional vaapiSupport libva
     ;
 
-# There are almost no need of "configure flags", but some libraries
-# weren't detected; see the TODO comments below
+  buildPhase = "python3 ${waf} build";
 
-  NIX_LDFLAGS = stdenv.lib.optionalString x11Support "-lX11 -lXext";
+  NIX_LDFLAGS = optionalString x11Support "-lX11 -lXext";
 
   enableParallelBuilding = true;
 
-  configurePhase = ''
-    python3 ${waf} configure --prefix=$out ${lib.optionalString vaapiSupport "--enable-vaapi"}
-    patchShebangs TOOLS
-  '';
-
-  buildPhase = ''
-    python3 ${waf} build
-  '';
-
   installPhase = ''
     python3 ${waf} install
+
     # Maybe not needed, but it doesn't hurt anyway: a standard font
     mkdir -p $out/share/mpv
     ln -s ${freefont_ttf}/share/fonts/truetype/FreeSans.ttf $out/share/mpv/subfont.ttf
-    '';
+  '';
 
-  meta = with stdenv.lib;{
+  meta = with stdenv.lib; {
     description = "A movie player that supports many video formats (MPlayer and mplayer2 fork)";
     longDescription = ''
       mpv is a free and open-source general-purpose video player,
@@ -121,7 +128,7 @@ stdenv.mkDerivation rec {
     '';
     homepage = http://mpv.io;
     license = licenses.gpl2Plus;
-    maintainers = with stdenv.lib.maintainers; [ AndersonTorres fuuzetsu ];
+    maintainers = with maintainers; [ AndersonTorres fuuzetsu ];
     platforms = platforms.linux;
   };
 }
