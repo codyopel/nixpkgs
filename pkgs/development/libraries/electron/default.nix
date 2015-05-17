b7c91eca2f653d684fa2b575043daa629d4eada8
codyopel <codyopel@gmail.com>
2015-05-17 17:46:24 -0400

    stash

--- pkgs/development/libraries/electron/default.nix
+++ pkgs/development/libraries/electron/default.nix
@@ -1,32 +1,71 @@
-{ stdenv, fetchurl, makeDesktopItem, makeWrapper, ninja, python
+{ stdenv, fetchgit, makeDesktopItem, makeWrapper, ninja, python, python26
 , alsaLib, atk, cairo, cups, dbus, glib, expat, fontconfig, freetype #, gconf
 , gdk_pixbuf, gtk, libcap, libgnome_keyring3, libgpgerror, nspr, nss, pango
 , xlibs, zlib
+, pythonPackages, git
 }:
+
+/* Maintainer notes:
+ *
+ * The Atom developers do not understand how to write a build system or
+ * python for that matter. Instead of using their broken python scripts
+ * call the build utilities manually.
+ */
+
+let
+  inherit (stdenv) isx86_64;
+in
+
+# Currently 32bit arches such as i686 & ARM32 are not supported
+# https://github.com/atom/electron/issues/366
+assert isx86_64;
 
 stdenv.mkDerivation rec {
   name = "electron-${version}";
   version = "0.26.0";
 
-  src = fetchurl {
-    url = "https://github.com/atom/electron/archive/v${version}.tar.gz";
-    sha256 = "1bwpc8fx9xz3l22fn6dq8sgf6qflmfn3gqdbxfx2viq74jqm8yrn";
+  src = fetchgit {
+    url = "https://github.com/atom/electron.git";
+    rev = "827741a9c602bc3b811bf56f61322a50ae9e882f";
+    sha256 = "1rzv15wx9lflngsmy6bs5prg150lsw43rfz1df16cjvrgx473mn3";
+    fetchSubmodules = true;
   };
 
   patchPhase = ''
-    patchShebangs .
+    patchShebangs ./script/
+    patchShebangs ./tools/
+    #patchShebangs ./vendor/depot_tools/gym_main.py
+    echo
+    echo
+    echo
+    echo
+    echo
+    ls vendor/depot_tools
   '';
 
-  nativeBuildInputs = [ ninja python ];
-
-  buildInputs = [
-    alsaLib atk cairo cups dbus glib expat fontconfig freetype
-    gdk_pixbuf gtk libcap libgnome_keyring3 libgpgerror nspr nss pango
-    xlibs zlib
+  nativeBuildInputs = [
+    ninja python python26 pythonPackages.gyp
   ];
 
+  buildInputs = [ git
+    alsaLib atk cairo cups dbus glib expat fontconfig freetype
+    gdk_pixbuf gtk libcap libgnome_keyring3 libgpgerror nspr nss pango
+    xlibs.libXrender xlibs.libX11 xlibs.libXext xlibs.libXdamage xlibs.libXtst
+    xlibs.libXcomposite xlibs.libXi xlibs.libXfixes xlibs.libXrandr
+    xlibs.libXcursor
+    zlib
+  ];
+
+  configureFlags = [
+    "-Dlibrary=static_library"
+  ];
+
+  configurePhase = ''
+    gyp -f ninja --depth . atom.gyp -Icommon.gypi $configureFlags
+  '';
+
   buildPhase = ''
-    ./script/build.py
+    ninja -C out/Release -j$NIX_BUILD_CORES -l$NIX_BUILD_CORES electron
   '';
 
   meta = with stdenv.lib; {
@@ -34,6 +73,6 @@ stdenv.mkDerivation rec {
     homepage = http://electron.atom.io/;
     license = licenses.mit;
     maintainers = with maintainers; [ codyopel ];
-    platforms = platforms.all;
+    platforms = [ "x86_64-linux" ];
   };
 }
